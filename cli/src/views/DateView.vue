234e3e30a072c572dd6f3fdf00ea67186cb225aa
llbigbroll <lldaybreakll@gmail.com>
2022-08-30 03:25:04 +0900

    소스정리

--- cli/src/views/DateView.vue
+++ cli/src/views/DateView.vue
@@ -6,7 +6,6 @@
 
 <script>
     export default { 
-        /* pdtmc^2w */
         props: [] ,
         data: function() {
             return {
@@ -24,19 +23,16 @@
             console.log("updated");
         },
         components: {
-            /* 컴포넌트 등록. 예시) "태그명" : 컴포넌트명 */
-            
+
         },
         computed: {
-            /* 자동처리 + 동기식. 메서드로 작성. return 필수. */
-            
+
         },
         watch: {
-            /* 자동처리 + 비동기식. data 에 등록된 프로퍼티 모니터링. 메서드로 작성. 매개변수 입력 필수  */
-            
+
         },
         methods: {
-            
+
         },
     }
 </script>
